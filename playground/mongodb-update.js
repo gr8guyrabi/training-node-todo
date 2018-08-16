6dde7902c7b2863bd27f0169a5f6f87415d05ec7
Rabi Maharjan <gr8guyrabi@gmail.com>
2018-08-16 08:38:32 +0545

    Update Script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,43 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true}, (err, client) => {
+  if(err) {
+    return console.log('Unable to connect to MongoDB server.');
+  }
+  
+  console.log('Connected to MongoDB server.');
+
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').findOneAndUpdate({ _id: new ObjectID('5b74d95a4baf165f0535705f') },{
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(JSON.stringify(result, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to update todo.');
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5b74d09b4baf165f05357057')
+  },{
+    $set: {
+      name: 'JavaScript'
+    },
+    $inc: {
+      age: 1
+    }
+  },{
+    returnOriginal: false
+  }).then((result) => {
+    console.log(JSON.stringify(result, undefined, 2))
+  }, (err) => {
+    console.log('Unable to update user.');
+  });
+
+  // client.close();
+
+});
