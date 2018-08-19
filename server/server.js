ca37a3ac039dc577b41b021c9dfb188a901843de
Rabi Maharjan <gr8guyrabi@gmail.com>
2018-08-19 21:45:54 +0545

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -91,7 +92,7 @@ app.delete('/todos/:id', (req, res) => {
     return res.status(404).send();
   }
 
-  Todo.findByIdAndRemove(id).then((todo) => {
+  Todo.findByIdAndRemove(id).then((todo) => { 
     if(!todo) {
       return res.status(404).send();
     }
@@ -116,6 +117,36 @@ app.delete('/users/:id', (req, res) => {
   });
 });
 
+
+app.patch('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {
+    $set: body
+  }, { new: true }).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+
+    return res.send({ todo });
+
+  }).catch((err) => {
+    return res.status(400).send();
+  })
+
+});
 
 
 app.listen(port, () => {
