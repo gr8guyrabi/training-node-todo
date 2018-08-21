479a6cf42ea6dfa04643d09e7eed073e37291fcc
Rabi Maharjan <gr8guyrabi@gmail.com>
2018-08-21 21:02:20 +0545

    Heroku && Test && Development database setup - fix

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://nodeTodos:Todos@123@ds123532.mlab.com:23532/node-todos-app', { useNewUrlParser: true });
 
-mongoose.connect(DATABASE || process.env.MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect( DATABASE || process.env.MONGODB_URI, { useNewUrlParser: true });
 
 mongoose.Promise = global.Promise;
 
